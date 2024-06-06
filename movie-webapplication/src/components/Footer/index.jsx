b2d6852db7a5ada4da30ade5a4d5bc4fb57b8bfe
InduwaraRBS <induwarakanchana567@gmail.com>
2024-06-06 16:40:57 +0530

    Second Commit

--- movie-webapplication/src/components/Footer/index.jsx
+++ movie-webapplication/src/components/Footer/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import "./Footer.css"
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 const index = () => {
   return (
-    <div><h1>Footer</h1></div>
+    <div className='Footer-Section'>
+      <div className='Footer-Left'>
+
+          <span>IT Group</span><br/>
+          <span>C.Salvador de Madariaga,1</span><br/>
+          <span>28027 Madrid</span><br/>
+          <span>Spain</span>
+      </div>
+      <div className='Footer-Right'>
+          <span>Follow us on</span>
+          <TwitterIcon/>
+          <YouTubeIcon/>
+      </div>
+          </div>
   )
 }
 
